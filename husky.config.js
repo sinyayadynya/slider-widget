b07cc7eca06e50eff9b67d5d6cbdc346aed97766
Sinyaya Dynya <sinyayadynya@gmail.com>
2022-10-28 02:55:28 +0600

    Update husky.config.js

--- husky.config.js
+++ husky.config.js
@@ -2,6 +2,6 @@ module.exports = {
   hooks: {
     'commit-msg  --no-verify': 'commitlint -e $HUSKY_GIT_PARAMS',
     'pre-commit': 'lint-staged',
-    'pre-push --no-verify': 'npm run-script test:ci',
+    'pre-push --no-verify': 'npm run-script',
   },
 };
